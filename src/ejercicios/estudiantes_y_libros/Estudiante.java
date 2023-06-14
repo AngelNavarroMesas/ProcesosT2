461e1d54c2ea7a174a0ef230abf690d154ffb945
angelnavarromesas <angelnavarromesas02@gmail.com>
2023-06-14 20:54:17 +0200

    comit filosofos arreglar palillo extra

--- src/ejercicios/estudiantes_y_libros/Estudiante.java
+++ src/ejercicios/estudiantes_y_libros/Estudiante.java
@@ -1,36 +1,56 @@
 package ejercicios.estudiantes_y_libros;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class Estudiante implements Runnable {
-    public static void main(String[] args) {
-        Estudiante e = new Estudiante();
-        for (int i = 1; i <= 4; i++) {
-            Thread hilo = new Thread(e);
-            hilo.setName("Estudiante " + i);
-            hilo.start();
-        }
-    }
+public class Estudiante implements Runnable{
+    public static List<Libro> listaLibros=new ArrayList<>();
+    public boolean terminado;
 
     @Override
     public void run() {
         try {
-            int libro1, libro2;
-            while (true) {
-                libro1 = new Random().nextInt(9);
-                libro2 = new Random().nextInt(9);
-                while (libro2 == libro1) {
-                    libro2 = new Random().nextInt(9);
+            Libro libro1=listaLibros.get((int)(Math.random()*9));
+            Libro libro2=listaLibros.get((int)(Math.random()*9));
+            synchronized (libro1) {
+                while (!terminado && libro1.isEnPosesion()) {
+                    libro1.wait();
                 }
-                Libro libro = new Libro(libro1, libro2);
-                libro.reservaLibros();
-                Thread.sleep((long) (Math.random() * 5000 + 3000));
-                libro.liberaLibros();
-                System.out.println(Thread.currentThread().getName() + " ha terminado de leer");
-                Thread.sleep(1000);
+                libro1.setEnPosesion(true);
+                System.out.println(Thread.currentThread().getName()+" esta usando el "+libro1.getName());
+                Thread.sleep(((long) Math.random()*2000)+3000);
+                System.out.println(Thread.currentThread().getName()+" ha terminado de usar el"+libro1.getName());
+                terminado = true;
+                libro1.setEnPosesion(false);
+                libro1.notifyAll();
+            }
+            synchronized (libro2) {
+                while (!terminado && libro2.isEnPosesion()) {
+                    libro1.wait();
+                }
+                libro2.setEnPosesion(true);
+                System.out.println(Thread.currentThread().getName()+" esta usando el "+libro2.getName());
+                Thread.sleep(((long) Math.random()*2000)+3000);
+                System.out.println(Thread.currentThread().getName()+" ha terminado de usar el "+libro2.getName());
+                terminado = true;
+                libro2.setEnPosesion(false);
+                libro2.notifyAll();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i<9; i++){
+            listaLibros.add(new Libro("Libro "+i));
+        }
+        for (int i = 0; i < 4; i++) {
+            Estudiante ewn = new Estudiante();
+            Thread hilo = new Thread(ewn);
+            hilo.setName("Estudiante "+i);
+            hilo.start();
         }
     }
 }
--- src/ejercicios/estudiantes_y_libros/Libro.java
+++ src/ejercicios/estudiantes_y_libros/Libro.java
@@ -1,34 +1,22 @@
 package ejercicios.estudiantes_y_libros;
 
-import java.util.Random;
-
 public class Libro {
-    public static boolean[] libros = new boolean[9];
-    int libro1, libro2;
-    public Libro() {}
+    public boolean enPosesion = false;
+    private String name;
 
-    public Libro(int libro1, int libro2) {
-        this.libro1 = libro1;
-        this.libro2 = libro2;
+    public Libro(String name){
+        this.name=name;
     }
 
-    public synchronized void reservaLibros() {
-        while (libros[libro1] == true || libros[libro2] == true|| libro1==libro2) {
-            while (libros[libro1]==true) {
-                libro1 = new Random().nextInt(9);
-            }
-            while (libros[libro2]==true) {
-                libro2 = new Random().nextInt(9);
-            }
-        }
-        System.out.println(Thread.currentThread().getName() + " tiene reservados los libros " + libro1 + " y " + libro2);
-        libros[libro1] = true;
-        libros[libro2] = true;
+    public String getName() {
+        return name;
     }
 
-    public synchronized void liberaLibros() {
-        libros[libro1] = false;
-        libros[libro2] = false;
-        this.notifyAll();
+    public boolean isEnPosesion() {
+        return enPosesion;
+    }
+
+    public void setEnPosesion(boolean enPosesion) {
+        this.enPosesion = enPosesion;
     }
 }
