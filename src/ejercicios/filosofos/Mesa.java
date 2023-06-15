e39dc44a1d42b074b03628b3f22daaa517418905
AngelNavarroMesas <angel.navarro@iesnervion.es>
2023-06-15 10:39:09 +0200

    fin creo

--- src/ejercicios/filosofos/Mesa.java
+++ src/ejercicios/filosofos/Mesa.java
@@ -1,34 +1,34 @@
 package ejercicios.filosofos;
 
 public class Mesa {
-    private boolean[] tenedores;
+    private boolean[] palillos;
     public Mesa(int numTenedores){
-        this.tenedores = new boolean[numTenedores];
+        this.palillos = new boolean[numTenedores];
     }
     public int PalilloIzquierda(int i){
         return i;
     }
     public int PalilloDerecha(int i){
         if(i == 0){
-            return this.tenedores.length - 1;
+            return this.palillos.length - 1;
         }else{
             return i - 1;
         }
     }
     public synchronized void cogerPalillos(int comensal){
-        while(tenedores[PalilloIzquierda(comensal)] || tenedores[PalilloDerecha(comensal)]){
+        while(palillos[PalilloIzquierda(comensal)] || palillos[PalilloDerecha(comensal)]){
             try {
                 wait(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        tenedores[PalilloIzquierda(comensal)] = true;
-        tenedores[PalilloDerecha(comensal)] = true;
+        palillos[PalilloIzquierda(comensal)] = true;
+        palillos[PalilloDerecha(comensal)] = true;
     }
     public synchronized void soltarPalillos(int comensal){
-        tenedores[PalilloIzquierda(comensal)] = false;
-        tenedores[PalilloDerecha(comensal)] = false;
+        palillos[PalilloIzquierda(comensal)] = false;
+        palillos[PalilloDerecha(comensal)] = false;
         this.notifyAll();
     }
 }
